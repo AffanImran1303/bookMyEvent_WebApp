d066b9d1f5b43b2ef80a07a38a4c565e20e6025c
Affan Imran <affanimran1303@gmail.com>
2025-01-31 04:37:51 +0500

    Fixed ESLint & MongoDB validation issues

--- src/lib/database/models/user.model.ts
+++ src/lib/database/models/user.model.ts
@@ -1,6 +1,6 @@
 import { Schema, model, models } from 'mongoose';
 const UserSchema = new Schema({
-    _id: { type: String, required: true, unique: true },
+    clerkId: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     username: { type: String, required: true, unique: true },
     firstName: { type: String, required: true },
@@ -9,16 +9,5 @@ const UserSchema = new Schema({
 })
 
 const User = models.User || model('User', UserSchema);
-
-const testUser = new User({
-    clerkId: "user_2sMoBzt44Ze5BjPEc6kVoe7KEJy",
-    email: "test@example.com",
-    username: "testuser",
-    firstName: "John",
-    lastName: "Doe",
-    photo: "https://example.com/photo.jpg"
-  });
-  await testUser.save();
-  console.log("User created:", testUser);
 
 export default User;
