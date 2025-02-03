78cb2e8ca2738cd888c1333c85c20467da925acb
Affan Imran <affanimran1303@gmail.com>
2025-02-03 23:27:04 +0500

    Fix deployment issues (4)

--- src/app/(root)/events/[id]/update/page.tsx
+++ src/app/(root)/events/[id]/update/page.tsx
@@ -1,5 +1,4 @@
 import EventForm from "@/components/shared/EventForm";
-import EventFrom from "@/components/shared/EventForm"
 import { getEventById } from "@/lib/actions/event.actions"
 import { UpdateEventProps } from "@/types";
 import {auth} from "@clerk/nextjs/server"
